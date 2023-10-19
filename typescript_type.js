bb8ba558b8e675f39cf8b67ceff7769e2f85b2ed
sourov <sourovsh533@gmail.com>
2023-10-19 14:15:10 +0600

    practice typescript type

---
+++ typescript_type.js
@@ -0,0 +1,7 @@
+var firstName; // type string
+firstName = "Sourov";
+console.log(typeof firstName);
+var num = 10;
+console.log(typeof num);
+var is_true = true;
+console.log(typeof is_true);
---
+++ typescript_type.ts
@@ -0,0 +1,9 @@
+let firstName: string; // type string
+firstName = "Sourov"
+console.log(typeof firstName);
+
+let num: number = 10;
+console.log(typeof num)
+
+let is_true: boolean = true;
+console.log(typeof is_true)
