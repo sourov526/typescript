a2654e0f7bae094d87c22afcd9fd3f84b37a7a65
sourov <sourovsh533@gmail.com>
2023-10-21 13:40:29 +0600

    add named tuples

--- tuples.js
+++ tuples.js
@@ -10,3 +10,11 @@ ourTuples = [5, false, 'Coding God was here'];
 // We have no type safety in our tuple for indexes 3+
 ourTuples.push('Something new and wrong');
 console.log(ourTuples);
+// Named tuples allow us to provide context for our values at each index
+var graph = [55.2, 41.3];
+console.log("print the graph here : ", graph);
+// Since tuples are arrays we can also destructure them.
+var graphs = [55.2, 41.3];
+var x = graphs[0], y = graphs[1];
+console.log("print x here : ", x);
+console.log("print y here : ", y);
--- tuples.ts
+++ tuples.ts
@@ -13,3 +13,14 @@ ourTuples = [5, false, 'Coding God was here'];
 // We have no type safety in our tuple for indexes 3+
 ourTuples.push('Something new and wrong');
 console.log(ourTuples);
+
+// Named tuples allow us to provide context for our values at each index
+const graph: [x: number, y: number] = [55.2, 41.3];
+console.log("print the graph here : ", graph)
+
+// Since tuples are arrays we can also destructure them.
+const graphs: [number, number] = [55.2, 41.3];
+const [x, y] = graphs;
+
+console.log("print x here : ", x)
+console.log("print y here : ", y)
