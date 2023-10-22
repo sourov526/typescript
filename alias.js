c3641abcef1dea2e048cf31249da293a9a79c003
sourov <sourovsh533@gmail.com>
2023-10-22 13:33:59 +0600

    add alias

---
+++ alias.js
@@ -0,0 +1,10 @@
+// console.log(Car)
+var carYear = 2001;
+var carType = "Toyota";
+var carModel = "Corolla";
+var car = {
+    year: carYear,
+    type: carType,
+    model: carModel
+};
+console.log(car);
---
+++ alias.ts
@@ -0,0 +1,23 @@
+// Type Aliases allow defining types with a custom name (an Alias).
+// Type Aliases can be used for primitives like string or more complex types such as objects and arrays
+// Try creating a new Car using the alias provided
+type CarYear = number;
+type CarType = string;
+type CarModel = string;
+type Car = {
+  year: CarYear,
+  type: CarType,
+  model: CarModel
+};
+// console.log(Car)
+
+const carYear: CarYear = 2001
+const carType: CarType = "Toyota"
+const carModel: CarModel = "Corolla"
+const car: Car = {
+  year: carYear,
+  type: carType,
+  model: carModel
+};
+
+console.log(car);
